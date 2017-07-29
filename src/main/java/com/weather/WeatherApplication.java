3a944008d45c22e13c3d9e4dc11df303af9a3b16
GoDonghyun <godong9@gmail.com>
2017-07-29 11:50:04 +0900

    Init project settings

--- src/main/java/com/weather/WeatherApplication.java
+++ src/main/java/com/weather/WeatherApplication.java
@@ -1,12 +1,16 @@
 package com.weather;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class WeatherApplication {
-
 	public static void main(String[] args) {
+		String property = System.getProperty("spring.profiles.active");
+		log.debug("spring.profiles.active: {}", property);
+
 		SpringApplication.run(WeatherApplication.class, args);
 	}
 }
---
+++ src/main/java/com/weather/config/ApplicationConfig.java
@@ -0,0 +1,37 @@
+package com.weather.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by godong9 on 2017. 7. 29..
+ */
+
+@EnableAsync
+@EnableJpaAuditing
+@Configuration
+public class ApplicationConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
+    }
+
+    @Bean
+    @Autowired
+    public RestTemplate restTemplate(ObjectMapper jacksonObjectMapper) {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().removeIf(converter -> converter instanceof MappingJackson2HttpMessageConverter);
+        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter(jacksonObjectMapper));
+        return restTemplate;
+    }
+}
