695bc3e570b4af33789b0d96b2ed269ea8e369cd
GoDonghyun <godong9@gmail.com>
2017-07-29 14:34:08 +0900

    Get Posts API 수정

--- src/main/java/com/weather/domain/post/PostController.java
+++ src/main/java/com/weather/domain/post/PostController.java
@@ -30,6 +30,8 @@ public class PostController {
      * @apiName PostCreate
      * @apiGroup Post
      *
+     * @apiDescription 포스트 생성
+     *
      * @apiParam {Number} prediction_id 기상청 예측 id
      * @apiParam {Number} user_id 유저 id
      * @apiParam {Number} weather_code 날씨 코드
@@ -55,6 +57,31 @@ public class PostController {
         return postResult;
     }
 
+    /**
+     * @api {get} /posts Request Post list
+     * @apiName PostList
+     * @apiGroup Post
+     *
+     * @apiDescription nx, ny에 존재하는 포스트를 배열 형태로 내려줌
+     *
+     * @apiParam {Number} nx X 좌표
+     * @apiParam {Number} ny Y 좌표
+     *
+     * @apiSuccess {Number} id 포스트 id
+     * @apiSuccess {Number} prediction_id 기상청 예측 id
+     * @apiSuccess {String} prediction_temperature 기상청 예측 기온
+     * @apiSuccess {String} prediction_humidity 기상청 예측 습도
+     * @apiSuccess {String} prediction_rain_prop 기상청 예측 강수확률
+     * @apiSuccess {Number} user_id 유저 id
+     * @apiSuccess {String} user_nicknam 유저 닉네임
+     * @apiSuccess {Number} code 날씨 코드
+     * @apiSuccess {String} text 댓글
+     * @apiSuccess {Number} nx X 좌표
+     * @apiSuccess {Number} ny Y 좌표
+     * @apiSuccess {Date} base_date 기상청 발표 시각
+     * @apiSuccess {Date} prediction_date 기상청 예보 시각
+     * @apiSuccess {Date} created_at 생성 시각
+     */
     @GetMapping("/posts")
     public List<PostResult> getPosts(@RequestParam("nx") Integer nx, @RequestParam("ny") Integer ny) {
         List<Post> postList = postService.findByNxAndNy(nx, ny);
@@ -73,6 +100,8 @@ public class PostController {
 //                    postResult.setPredictionTemperature(prediction.getTemperature());
 //                    postResult.setPredictionHumidity(prediction.getHumidity());
 //                    postResult.setPredictionRainProp(prediction.getRainProp());
+//                    postResult.setBaseDate(prediction.getBaseDate());
+//                    postResult.setPredictionDate(prediction.getPredictionDate());
 
                     return postResult;
                 })
